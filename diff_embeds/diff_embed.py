79cbcfbc7f347c275be5c33b3a79aa4ed2518291
kareef928 <kareef928@gmail.com>
2019-08-05 16:04:56 -0400

    clustering

--- diff_embeds/diff_embed.py
+++ diff_embeds/diff_embed.py
@@ -24,10 +24,10 @@ def decay_q(slope, n):
 
 #Static Variables
 slope = 100
-p = 0.3
-n_verts = [200, 300, 400, 500]
+p = 0.75
+n_verts = [200, 300, 400]
 n_sims = 1
-embed = LaplacianSpectralEmbed()
+embed = AdjacencySpectralEmbed()
 
 #Generate B Matrix
 def B_matrix(k, p, q):
@@ -42,10 +42,43 @@ for n in n_verts:
         k = linear_k(slope, n)
         q = decay_q(slope, n)
         B = B_matrix(k, p, q)
-        cn = [n // k] * k
-        node_labels = n_to_labels(cn).astype(int)
+        cn = [n//k] * k
+        labels_sbm = n_to_labels(cn).astype(int)
         G = sbm(cn, B)
-        heatmap(G, title=f"k={k} q={q}", inner_hier_labels=node_labels)
+        heatmap(G, title=f"k={k} q={q}", inner_hier_labels=labels_sbm)
+
+        #embedding
         Xhat = embed.fit_transform(G)
-        pairplot(Xhat, title="Laplacian Spectral Embedding")
+        pairplot(Xhat, title="Adjacency Spectral Embedding")
+
+        #clustering
+        clust = KMeans(n_clusters = k)
+        palette = {'Right':(0,0.7,0.2),
+                   'Wrong':(0.8,0.1,0.1)}
+        labels_clust = clust.fit_predict(Xhat)
+        ari = adjusted_rand_score(labels_sbm, labels_clust)
+        print(labels_sbm)
+        print(labels_clust)
+        error = labels_sbm - labels_clust
+        error = error != 0
+        if np.sum(error) / (n) > 0.5:
+                error = error == 0
+        error_rate = np.sum(error) / (n)
+        error_label = (n) * ["Right"]
+        error_label = np.array(error_label)
+        error_label[error]= "Wrong"
+
+        pairplot(Xhat,
+                 labels=labels_clust,
+                 title='KMeans on embedding, ARI: {}'.format(str(ari)[:5]),
+                 legend_name='Predicted label',
+                 height=3.5,
+                 palette='muted')
         
+        pairplot(Xhat,
+                 labels=error_label,
+                 title='Error from KMeans, Error rate: {}'.format(str(error_rate)),
+                 legend_name='Error label',
+                 height=3.5,
+                 palette=palette)
+        plt.show()
