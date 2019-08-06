ecc190afc77f97295fd145bf3686a37b88f6739d
bdpedigo <benjamindpedigo@gmail.com>
2019-08-06 08:25:20 -0400

    try hsbm

--- try_ldt_dros.py/try_ldt_dros.py
+++ try_ldt_dros.py/try_ldt_dros.py
@@ -1,13 +1,7 @@
 from graspy.datasets import load_drosophila_left, load_drosophila_right
 from graspy.inference import LatentDistributionTest
-
 left_graph = load_drosophila_left()
 right_graph = load_drosophila_right()
-
 ldt = LatentDistributionTest(n_components=3, n_bootstraps=500)
-
-ldt.fit(left_graph, right_graph)
-
-p_value = ldt.p_
-
-print(f"p-value from latent distribution test: {p_value}")
+p_value = ldt.fit(left_graph, right_graph)
+print("p-value: " + str(p_value))
