d27bbef3120bc471ea3268490b1c09d46a3f1923
kareef928 <kareef928@gmail.com>
2019-08-06 15:34:13 -0400

    avg ARI and standard error

--- diff_embeds/diff_embed.py
+++ diff_embeds/diff_embed.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np 
-import seaborn as sns 
-import pandas as pd
+import seaborn as sns
+from scipy import stats
 from graspy.simulations import sbm
 from graspy.embed import AdjacencySpectralEmbed, LaplacianSpectralEmbed
 from graspy.plot import heatmap, pairplot
@@ -23,15 +23,6 @@ def decay_q(slope, n):
     q = slope / n
     return q
 
-#Static Variables
-slope = 50
-p = 0.5
-n_verts = [100, 150, 200, 250, 300]
-n_sims = 3
-embed = AdjacencySpectralEmbed()
-temp = []
-ari_vals = []
-
 #Generate B Matrix
 def B_matrix(k, p, q):
     B = np.zeros((k,k))
@@ -39,29 +30,50 @@ def B_matrix(k, p, q):
     B[B == 0] = q
     return B
 
-#Generate graph
-for n in n_verts:
-    for _ in range(n_sims):
-        k = linear_k(slope, n)
-        q = decay_q(slope, n)
-        B = B_matrix(k, p, q)
-        cn = [n//k] * k
-        labels_sbm = n_to_labels(cn).astype(int)
-        G = sbm(cn, B)
+def avg_ari(slope, n_verts, n_sims, p, embed):
+    temp = []
+    ari_vals = []
+    stand_error = []
+    #Generate graph
+    for n in n_verts:
+        for _ in range(n_sims):
+            k = linear_k(slope, n)
+            q = decay_q(slope, n)
+            B = B_matrix(k, p, q)
+            cn = [n//k] * k
+            labels_sbm = n_to_labels(cn).astype(int)
+            G = sbm(cn, B)
 
-        #embedding
-        Xhat = embed.fit_transform(G)
+            #embedding
+            Xhat = embed.fit_transform(G)
 
-        #clustering
-        clust = KMeans(n_clusters = k)
-        labels_clust = clust.fit_predict(Xhat)
-        ari = adjusted_rand_score(labels_sbm, labels_clust)
-        temp.append(ari)
-    ari_vals.append(temp)
-    temp = []     
+            #clustering
+            clust = KMeans(n_clusters = k)
+            labels_clust = clust.fit_predict(Xhat)
+            ari = adjusted_rand_score(labels_sbm, labels_clust)
+            temp.append(ari)
+            print("{}: {}".format(n, ari))
+        ari_vals.append(np.sum(temp) / n_sims)
+        stand_error.append(stats.sem(temp))
+        temp = []     
+    return(ari_vals, stand_error)
 
-for xe, ye in zip(n_verts, ari_vals):
-    plt.scatter([xe] * len(ye), ye)
+#Variables
+slope = 25
+p = 0.5
+n_verts = [50, 75, 100, 125, 150, 175, 200]
+n_sims = 35
+embed = AdjacencySpectralEmbed()
+
+ari_vals, stand_error = avg_ari(slope, n_verts, n_sims, p, embed)
+print(ari_vals)
+print(stand_error)
+plt.errorbar(n_verts, 
+             ari_vals, 
+             yerr=stand_error,
+             marker='s',
+             mfc='red',
+             mec='green')
 plt.title("kchange_qchange_ASE_KMeans")
 plt.xlabel("n_verts")
 plt.xticks(n_verts)
