3fcf11aa51c6fcb71ef9a297b757b8ba6f0744b2
bdpedigo <benjamindpedigo@gmail.com>
2019-09-02 20:05:55 -0400

    testing gclust

--- test_gclust/test_gclust.py
+++ test_gclust/test_gclust.py
@@ -3,10 +3,10 @@ from graspy.cluster import GaussianCluster
 from graspy.plot import pairplot
 import numpy as np
 
-np.random.seed(3)
-
 n = 100
 d = 3
+
+np.random.seed(3)
 
 X1 = np.random.normal(0.5, 0.5, size=(n, d))
 X2 = np.random.normal(-0.5, 0.5, size=(n, d))
@@ -15,6 +15,8 @@ X4 = np.random.uniform(0.2, 0.3, size=(n, d))
 X = np.vstack((X1, X2, X3, X4))
 pairplot(X)
 
+np.random.seed(3)
+
 gclust = GaussianCluster(min_components=2, max_components=2, n_init=1, max_iter=100)
 gclust.fit(X)
 
@@ -22,11 +24,12 @@ bic1 = gclust.bic_
 
 np.random.seed(3)
 
-gclust = GaussianCluster(min_components=2, max_components=2, n_init=3, max_iter=100)
+gclust = GaussianCluster(min_components=2, max_components=2, n_init=50, max_iter=100)
 gclust.fit(X)
 
 bic2 = gclust.bic_
 
+# we'd hope that bic2 is a little bit lower than bic1
 print(bic1)
 print(bic2)
-#%%
+
