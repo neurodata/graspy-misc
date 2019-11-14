4c8282c8d793d9b1b7a6979d8f30572bcbce11b6
bdpedigo <benjamindpedigo@gmail.com>
2019-11-14 11:15:03 -0500

    new sims

--- diag-aug/diag_aug.py
+++ diag-aug/diag_aug.py
@@ -1,16 +1,20 @@
 # %% [markdown]
 # #
-from graspy.simulations import er_np
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
-
 from graspy.embed import AdjacencySpectralEmbed
+from graspy.simulations import er_np
 
-n_verts = 500
+# Experiment parameters
+
+n_verts = 200
 p = 0.5
 n_components = 1
-n_sims = 200
+n_sims = 1000
+
+
+# Run experiment
 
 estimated_latents = np.zeros((n_sims, 2))
 for i in range(n_sims):
@@ -30,6 +34,9 @@ for i in range(n_sims):
 
 diffs = estimated_latents - np.sqrt(p)  # the true latent position is sqrt(p)
 
+
+# Plot results
+
 plt.style.use("seaborn-white")
 sns.set_palette("deep")
 sns.set_context("talk", font_scale=1)
@@ -40,4 +47,12 @@ sns.distplot(diffs[:, 1], label="Without diagaug")
 plt.axvline(0, c="r", linestyle="--", label="True")
 plt.xlabel("Difference from true latent position")
 plt.legend()
-
+plt.title(f"ER graphs, p = {p}, {n_verts} vertices, {n_sims} simulations")
+plt.savefig(
+    "./graspy-misc/diag-aug/diag-aug-er.png",
+    fmt="png",
+    dpi=150,
+    facecolor="w",
+    bbox_inches="tight",
+    pad_inches=0.3,
+)
