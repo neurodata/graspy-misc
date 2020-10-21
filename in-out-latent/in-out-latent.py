5dbf11df5785fc9161228497a128aae11cc0d5c7
bdpedigo <benjamindpedigo@gmail.com>
2020-10-21 13:05:38 -0400

    update params

--- in-out-latent/in-out-latent.py
+++ in-out-latent/in-out-latent.py
@@ -3,7 +3,7 @@ import graspy as gs
 import numpy as np
 import seaborn as sns
 
-B = np.array([[0.3, 0.1, 0.1], [0.3, 0.1, 0.1], [0.1, 0.5, 0.3]])
+B = np.array([[0.3, 0.1, 0.1], [0.3, 0.1, 0.1], [0.1, 0.5, 0.1]])
 sns.heatmap(B, square=True, annot=True, cmap="RdBu_r", center=0)
 adj, labels = gs.simulations.sbm([100, 100, 100], B, return_labels=True, directed=True)
 ase = gs.embed.AdjacencySpectralEmbed(diag_aug=False, n_components=3)
